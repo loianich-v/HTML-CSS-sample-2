a7a727ad35d5a0d7135d7a1a8f57304a70927a9e
loianich-v <mailo92.v@gmail.com>
2024-11-26 16:27:42 +0200

    js-problem

--- js/modal.js
+++ js/modal.js
@@ -1,8 +1,8 @@
 const modal = document.querySelector('.backdrop');
-const modalBtnOpen = document.querySelector('.modal-btn-open');
-const modalBtnClose = document.querySelector('.modal-btn-close');
+const menuBtnOpen = document.querySelector('.modal-btn-open');
+const menuBtnClose = document.querySelector('.modal-btn-close');
 
 const toggleModal = () => modal.classList.toggle('is-hidden');
 
-modalBtnOpen.addEventListener('click', toggleModal);
-modalBtnClose.addEventListener('click', toggleModal);
+menuBtnOpen.addEventListener('click', toggleModal);
+menuBtnClose.addEventListener('click', toggleModal);
